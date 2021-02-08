e0f34df9a6fc2b9fb584a14510a6480c94908bae
tsaduranga <tsaduranga@gmail.com>
2021-02-08 22:20:03 +0530

    add larael 6 basic files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+
+
+        <h1 class="text-center">Welcome To Accounting System</h1>
+
+        <div style="margin-top:150px">
+            <h3 class="">Enter Bank Section: <a href="" class="btn btn-primary btn-lg"> Here </a></h3>
+        </div>
+
+
+
+    </div>
+
+@endsection
